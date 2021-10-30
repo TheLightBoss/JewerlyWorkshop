e7ef3f497deccf0d3f739856e8a89e86fa9cb9fa
saakiys <saak4ik@gmail.com>
2021-10-30 13:09:12 +0300

    reference product
    add & update

--- ServerApp/ServerApp/src/controllers/procedures.controller.js
+++ ServerApp/ServerApp/src/controllers/procedures.controller.js
@@ -2,33 +2,43 @@ const { json } = require('express');
 const db = require('../connection_db/db')
 class ProceduresController{
 
+    /**
+     * Adding an entry to the 'productions' 
+     */
     async createProduction(req, res){
-        const {id_prod, id_empl} = req.body;
+        const {id_empl, id_prod} = req.body;
         console.log("Requested method POST 'Production'");
         try{
-            await db.query('CALL public.insert_production($1, $2);', [id_prod, id_empl]);
+            await db.query('CALL public.insert_production($1, $2);', [id_empl, id_prod]);
+            console.log("Added an entry to the 'productions'");
             res.json(0);
         }
-        catch{
+        catch(e){
+            console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack);
             res.json(-1);
         }
     }
-
+    /**
+     * Updating an entry to the 'productions'
+     */
     async updateProduction(req, res){
         const {id_production, list_done, status, half_status} = req.body;
         console.log("Requested method PUT 'Production'");
-        //try-catch работает некорректно ИСПРАВИТЬ
         try{
             const result = await db.
             query('CALL public.update_production($1, $2, $3, $4);', 
             [id_production, list_done, status, half_status]);
+            console.log("Updated an entry to the 'productions' (id = " + id_production + ")");
             res.json(0);
         }
-        catch{
+        catch(e){
+            console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack);
             res.json(-1);
         }
     }
-    
+    /**
+     * Adding an entry to the 'employees' 
+     */
     async createEmployee(req, res){
         const {fio_new, address_new, passp, exp, log, passw} = req.body;
         const employees = await db.query('SELECT id_empl, login, password  FROM public.employees;');
@@ -36,17 +46,21 @@ class ProceduresController{
         console.log("Requested method POST 'Employee'");
         try{
             if(copyEmpl !== undefined){
-                throw new Error;
+                throw new Error('Login and password are not special');
             }
             await db.query('CALL public.insert_employee($1, $2, $3, $4, $5, $6);', 
             [fio_new, address_new, passp, exp, log, passw]);
+            console.log("Added an entry to the 'employees'");
             res.json(0);
         }
-        catch{
-            res.json("Login and password are not special");
+        catch(e){
+            console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack);
+            res.json(-1);
         }
     }
-
+    /**
+     * Updating an entry to the 'employees'
+     */
     async updateEmployee(req, res){
         const {idd, fio_new, address_new, passp, exp, log, passw} = req.body;
         const employees = await db.query('SELECT id_empl, login, password FROM public.employees;');
@@ -57,26 +71,53 @@ class ProceduresController{
             if(copyEmpl === undefined || copyEmpl['id_empl'] === idd ){
                 await db.query('CALL public.update_employee($1, $2, $3, $4, $5, $6, $7);', 
                 [idd, fio_new, address_new, passp, exp, log, passw]);
+                console.log("Updated an entry to the 'employees' (id = " + idd + ")");
                 res.json(0);                
             }
             else{
-                throw new Error;
+                throw new Error('Login and password are not special');
             }
         }
-        catch{
+        catch(e){
+            console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack);
             res.json(-1);
         }
     }
-    
+    /**
+     * Adding an entry to the 'reference_products' 
+     */
     async createReferenceProduct(req, res){
-        
+        const {name_pr, url, id_mat, name_it, id_dec, id_listt, weight} = req.body;
+        console.log("Requested method POST 'Reference Product'");
+        try{
+            await db.query('CALL public.insert_ref_product($1, $2, $3, $4, $5, $6, $7);', 
+            [name_pr, url, id_mat, name_it, id_dec, id_listt, weight]);
+            console.log("Added an entry to the 'reference_products'");
+            res.json(0);
+        }
+        catch(e){
+            console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack);
+            res.json(-1);
+        }
     }
-
+    /**
+     * Updating an entry to the 'reference_products'
+     */
     async updateReferenceProduct(req, res){
-        
+        const {idd, name_pr, url, id_mat, name_it, id_dec, id_listt, weight} = req.body;
+        console.log("Requested method PUT 'Reference Product'");
+        try{
+            await db.query('CALL public.update_ref_product($1, $2, $3, $4, $5, $6, $7, $8);', 
+            [idd, name_pr, url, id_mat, name_it, id_dec, id_listt, weight]);
+            console.log("Updated an entry to the 'reference_products'");
+            res.json(0);
+        }
+        catch(e){
+            console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack);
+            res.json(-1);
+        }
     }
-
 }
 
-//экспортируем объект этого контроллера
+//export this object
 module.exports = new ProceduresController()
--- ServerApp/ServerApp/src/controllers/views.controller.js
+++ ServerApp/ServerApp/src/controllers/views.controller.js
@@ -1,10 +1,12 @@
 const db = require('../connection_db/db')
 
 class ViewsController{
-    /*
-    * Получение заказов для менеджерского приложения
+    /** 
+     * Getting orders for managment's app.
+     * if argument : new - to get new orders
+     * if argument : run - to get running orders
+     * else  
     */
-   //метод получился сложным позже ИЗМЕНИТЬ 
     async getOrders(req, res){
         const {argument} = req.body;
         console.log("Requested method GET 'Orders' " + argument);
@@ -14,24 +16,26 @@ class ViewsController{
             res.json(result);
         }
         else if(argument === "run"){
-            const result = all_orders.rows.filter(row => row['id_empl'] !== null);
+            const result = all_orders.rows.filter(row => row['id_empl'] !== null
+            && row['status'] !== true);
             res.json(result);
         }
         else{
-            res.json(all_orders.rows);
+            const result = all_orders.rows.filter(row => row['status'] === true);
+            res.json(result);
         }
     }
-    /*
-    * Получение заказов для клиентского приложения
+    /**
+     * Getting orders for client's app.
     */
     async getClientOrders(req, res){
         const {id} = req.body;
-        console.log("Requested method GET 'ClientOrders' id_client = " + id);
+        console.log("Requested method GET 'Client's orders' id_client = " + id);
         const client_orders = await db.query('SELECT * FROM client_orders');
         const result = client_orders.rows.filter(row => row['id_client'] == id);
         res.json(result);
     }
 }
 
-//экспортируем объект этого контроллера
+//export this object
 module.exports = new ViewsController()
